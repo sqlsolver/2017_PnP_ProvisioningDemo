b6b48b78754b382f2ec2a03181264e113c68c503
RamonaM <RamonaM@RAMONAM-PC3>
2017-09-20 21:16:26 -0700

    Added web part and taxonomies directories plus content.

---
+++ wp/MedPOC_wp/src/webparts/viewDoc/ViewDocWebPart.ts
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ViewDocWebPartStrings';
+import ViewDoc from './components/ViewDoc';
+import { IViewDocProps } from './components/IViewDocProps';
+import { IViewDocWebPartProps } from './IViewDocWebPartProps';
+
+export default class ViewDocWebPart extends BaseClientSideWebPart<IViewDocWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IViewDocProps > = React.createElement(
+      ViewDoc,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ wp/MedPOC_wp/src/webparts/viewDoc/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IViewDocWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ViewDocWebPartStrings' {
+  const strings: IViewDocWebPartStrings;
+  export = strings;
+}
